de24da742ea77c228286ebc3880ddd1a12bd6794
Vlad <84898137+Djkill1994@users.noreply.github.com>
2022-03-07 22:51:09 +0300

    project clone social networking Instagram, added components header, navbar

---
+++ src/component/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+import s from './navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <nav className={s.nav}>
+      <div>
+        nav 1
+      </div>
+      <div>
+        nav 2
+      </div>
+      <div>
+        nav 3
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
