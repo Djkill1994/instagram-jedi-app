ce9b0b2d440a5fcd6ad80e4263bd93a7faf9b39b
Vlad <84898137+Djkill1994@users.noreply.github.com>
2022-04-05 11:39:21 +0300

    feature/IG-14 Замечания устранены

--- src/features/Post/components/Post/HeaderPost/HeaderPost.tsx
+++ src/features/Post/components/Post/HeaderPost/HeaderPost.tsx
@@ -1,6 +1,7 @@
 import s from "./HeaderPost.module.scss";
 import React from "react";
 import { Avatar, Stack } from "@mui/material";
+import { ReactComponent as Additionally } from "../../../../../assets/svg/additionally.svg";
 
 interface HeaderPostProps {
   userAvatar: string;
@@ -22,20 +23,7 @@ export const HeaderPost: React.FC<HeaderPostProps> = ({
         <Avatar src={userAvatar} alt="Avatar" className={s.avatar} />
         <span className={s.nameAccount}>{userName}</span>
       </Stack>
-      <svg
-        aria-label="Дополнительно"
-        className="_8-yf5 "
-        color="#262626"
-        fill="#262626"
-        height="24"
-        role="img"
-        viewBox="0 0 24 24"
-        width="24"
-      >
-        <circle cx="12" cy="12" r="1.5" />
-        <circle cx="6" cy="12" r="1.5" />
-        <circle cx="18" cy="12" r="1.5" />
-      </svg>
+      <Additionally />
     </Stack>
   );
 };
