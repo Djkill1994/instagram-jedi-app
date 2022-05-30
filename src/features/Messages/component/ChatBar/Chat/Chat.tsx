9427a1ff577fc54ed3b43e9fa83943fe7fab6a8a
Vlad <84898137+Djkill1994@users.noreply.github.com>
2022-05-30 21:07:43 +0300

    feature/IG-3-2 реализовано взаимодействие с бэкэндом

--- src/features/Messages/component/ChatBar/Chat/Chat.tsx
+++ src/features/Messages/component/ChatBar/Chat/Chat.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import { Box, Stack } from "@mui/material";
 import s from "./Chat.module.scss";
 import { useChat } from "../../../hooks/useChat";
+import clsx from "clsx";
 
 export const Chat: React.FC = () => {
   const { messages } = useChat("1");
 
   return (
-    <Stack
-      className={s.chat}
-      spacing={1}
-      alignItems="baseline"
-      justifyContent="flex-end"
-    >
+    <Stack className={s.chat} spacing={1} alignItems="baseline">
       {messages.map(({ messageText, messageId, currentUser }) => (
         <Box
           key={messageId}
-          className={s.itemMessage}
-          style={{ color: currentUser ? "green" : "red" }}
+          className={clsx(
+            s.itemMessage,
+            currentUser ? s.otherUser : s.currentUser
+          )}
         >
           {messageText}
         </Box>
