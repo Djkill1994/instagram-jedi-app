bb427250d7b2cb137f9a2ab10644932c89512221
Vlad <84898137+Djkill1994@users.noreply.github.com>
2022-08-31 20:28:10 +0300

    IG-3-4: Переделаны стили кнопки , добавлен ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,45 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { App } from "./common/components/App";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const prepare = async (): Promise<void> => {
   const { worker } = await import("./mocks/browser");
   await worker.start({ onUnhandledRequest: "bypass" });
 };
 
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          height: "30px",
+          backgroundColor: "#0095f6",
+          textTransform: "none",
+          color: "white",
+          "&:hover": {
+            backgroundColor: "#0095f6",
+            opacity: "0.7",
+          },
+          "&:disabled": {
+            opacity: "0.3",
+            backgroundColor: "#0095f6",
+            color: "white",
+          },
+        },
+      },
+    },
+  },
+});
+
 prepare().then(() => {
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
         <BrowserRouter>
-          <App />
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
         </BrowserRouter>
       </Provider>
     </React.StrictMode>,
